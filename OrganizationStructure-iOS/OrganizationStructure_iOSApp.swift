817cfb0e2ae0bf71fe8d160e8f09a49f0ba88b9d
MinaSamir <menasamercap@gmail.com>
2025-12-27 11:34:54 +0200

    Initial Commit

---
+++ OrganizationStructure-iOS/OrganizationStructure_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizationStructure_iOSApp.swift
+//  OrganizationStructure-iOS
+//
+//  Created by Mina Samir on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct OrganizationStructure_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
